9c39d2873022fc6018e8fde48ecea6ae0d5bb7db
Lightening Gibson <lighteningibson@gmail.com>
2022-10-20 16:19:52 -0500

    Working on Fetching data from Json server

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ let clickHereDiv = document.getElementById('circle-text');
 
 clickHereDiv.addEventListener('click', () => clickHereDiv.remove());
 
+let dropDownList = document.getElementById('DropdownList');
+
+dropDownList.addEventListener('change', event => console.log(event.target.value));
+
 
 let buttons = document.querySelectorAll("button");
 
 function buttonActions(buttons) {
     buttons.forEach(button => 
-        button.addEventListener("click", event => console.log(event)));
-        return buttons.event;
-}
+        button.addEventListener("click", event => { 
+             if(event.target.value === dropDownList.event.target.value ) {
+                fetch("http://localhost:3000")
+                    .then(response => response.json())
+                        .then(data => )
+
+             }
+
+            console.log(event)
+            return event
+                                                }));}
 
 buttonActions(buttons);
 
