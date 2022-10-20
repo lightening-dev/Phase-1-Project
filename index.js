0052d82fb80aebb300e356e5676782c517c1b3d3
Lightening Gibson <lighteningibson@gmail.com>
2022-10-19 23:00:23 -0500

    Added Event Listener to Buttons

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ let clickHereDiv = document.getElementById('circle-text');
 clickHereDiv.addEventListener('click', () => clickHereDiv.remove());
 
 
-// let test = document.getElementById('categoryContainer');
+let buttons = document.querySelectorAll("button");
 
-// clickHereDiv.addEventListener("click", function(event){
-//    debugger
-//     console.log(event)
-// });
+function buttonActions(buttons) {
+    buttons.forEach(button => 
+        button.addEventListener("click", event => console.log(event)));
+        return buttons.event;
+}
 
+buttonActions(buttons);
 
 
 
