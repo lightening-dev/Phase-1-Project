9dd9e5a26d1cabb843d19815254e4a787047d70d
Lightening Gibson <lighteningibson@gmail.com>
2022-10-20 22:18:28 -0500

    finally have return values of list and buttons correct

--- index.js
+++ index.js
@@ -5,27 +5,35 @@ clickHereDiv.addEventListener('click', () => clickHereDiv.remove());
 
 let dropDownList = document.getElementById('DropdownList');
 
-dropDownList.addEventListener('change', event => console.log(event.target.value));
+dropDownList.addEventListener('change', event => {
+    event.preventDefault();
+    let dropDownListButton = event.target.value;
+    console.log(dropDownListButton)
+        return dropDownListButton}
+    ); 
 
+    
 
 let buttons = document.querySelectorAll("button");
 
 function buttonActions(buttons) {
     buttons.forEach(button => 
         button.addEventListener("click", event => { 
-             if(event.target.value === dropDownList.event.target.value ) {
-                fetch("http://localhost:3000")
-                    .then(response => response.json())
-                        .then(data => )
-
-             }
-
-            console.log(event)
-            return event
-                                                }));}
+             event.preventDefault();
+             
+             let buttonResult = event.target.value;
+             console.log(buttonResult)
+            return buttonResult;
+            
+                                                }))
+                                                ;
+                                            }
 
 buttonActions(buttons);
 
+// function compareValues(){
+//     if()
+// }
 
 
 
