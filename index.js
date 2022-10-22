94756a8f5e7e693e0b5a20c9be39b937ccf8ac42
Lightening Gibson <lighteningibson@gmail.com>
2022-10-22 15:59:22 -0500

    Finally have the Img tag and the src attibute on each img tag

--- index.js
+++ index.js
@@ -1,39 +1,79 @@
 
-let clickHereDiv = document.getElementById('circle-text');
+    const clickHereDiv = document.getElementById('circle-text');
 
-clickHereDiv.addEventListener('click', () => clickHereDiv.remove());
+    let selectedGenre = "";
+    let selectedMovieGenre = "";
 
-let dropDownList = document.getElementById('DropdownList');
+    const compareResults = (selectedMovieGenre) => {
+
+        console.log(selectedMovieGenre);
+    }
+//clickHereDiv.addEventListener('click', () => clickHereDiv.remove());
+
+    let dropDownList = document.getElementById('DropdownList');
+
+    let buttons = document.querySelectorAll("button");
+
+    let categoryContainer = document.getElementById('categorContainer');
 
 dropDownList.addEventListener('change', event => {
-    event.preventDefault();
-    let dropDownListButton = event.target.value;
-    console.log(dropDownListButton)
-        return dropDownListButton}
+        event.preventDefault();
+        let movieGenre = event.target.value;
+        console.log(movieGenre)
+        selectedMovieGenre = movieGenre;
+        //console.log(selectedMovieGenre)
+        return movieGenre}
     ); 
+    const Horror = document.getElementById('horrorHeader');
+    const Family = document.getElementById('familyHeader');
+    const Comedy = document.getElementById('comedyHeader');
+    const SciFi = document.getElementById('sci-fiHeader');
+        
+      
 
-    
-
-let buttons = document.querySelectorAll("button");
+    async function fetchMovies(id) {
+        const movieData = await fetch(`http://localhost:3000/movies/${id}`)
+        .then(response  => response.json())
+        .then(data => image = data.image)
+        
+        }
+        
+        
+       
 
 function buttonActions(buttons) {
-    buttons.forEach(button => 
-        button.addEventListener("click", event => { 
-             event.preventDefault();
-             
-             let buttonResult = event.target.value;
-             console.log(buttonResult)
-            return buttonResult;
-            
-                                                }))
-                                                ;
-                                            }
+        buttons.forEach(button => button.addEventListener("click", event => { 
+                  event.preventDefault();
+                  selectedGenre = event.target.value;
+                  if(selectedGenre === selectedMovieGenre) {
+                       let image = document.createElement('img');
+                       
+                      
+                       if(selectedGenre == "Horror") {
+                            image.src = fetchMovies(3);
+                            Horror.appendChild(image); 
+                        }
+                       else if (selectedGenre == "Family") {
+                            image.src = fetchMovies(1);
+                            Family.appendChild(image)
+                       }
+                       else if (selectedGenre == "Comedy") {
+                            image.src = fetchMovies(4);
+                            Comedy.appendChild(image)
+                       }
+                       else if (selectedGenre == "Sci-Fi") {
+                            image.src = fetchMovies(2);
+                            SciFi.appendChild(image);
+                       }
+                }
+                  else alert("Wrong genre, try again!")
+        }           )                )
+    }
 
-buttonActions(buttons);
+document.addEventListener('DOMContentLoaded', buttonActions(buttons));
 
-// function compareValues(){
-//     if()
-// }
+
+
 
 
 
